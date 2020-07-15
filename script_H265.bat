88f823e2490e49798d9ef3669021d472fa697b99
Jaedong Kim <jd.kim@kaistudio.co.kr>
2020-07-15 12:10:35 +0900

    add option to remove script

--- script_H265.bat
+++ script_H265.bat
@@ -1,6 +1,7 @@
 set width=3840
 for /F "tokens=*" %%A in (list.txt) do (
     ECHO %%A
-    ECHO 
-    ffmpeg -i %%A -c:v hevc_nvenc -b:v 25M -maxrate 30M -pix_fmt yuv420p -vf  scale=%width%:-1 %%~nA_H265.mp4
+    ffmpeg -i %%A -c:v copy -sn -dn -an %%~nA.ts
+    ffmpeg -i %%~nA.ts -c:v copy %%~nA_ns.mp4
+    ffmpeg -i %%~nA_ns.mp4 -c:v hevc_nvenc -b:v 25M -maxrate 30M -pix_fmt yuv420p -vf  scale=%width%:-1 %%~nA_H265.mp4
 )
