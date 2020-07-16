78208dff173aaeaed1990c38db2ce61d3d94a2ba
Jaedong Kim <jd.kim@kaistudio.co.kr>
2020-07-16 10:46:57 +0900

    remove temp file and add set codec when it makes ts file

--- script_H264.bat
+++ script_H264.bat
@@ -1,7 +1,9 @@
 set width=3840
 for /F "tokens=*" %%A in (list.txt) do (
     ECHO %%A
-    ffmpeg -i %%A -c:v copy -sn -dn -an %%~nA.ts
-    ffmpeg -i %%~nA.ts -c:v copy %%~nA_ns.mp4
-    ffmpeg -i %%~nA_ns.mp4 -c:v h264_nvenc -b:v 25M -maxrate 30M -pix_fmt yuv420p -vf scale=%width%:-1 %%~nA_H264.mp4
+    ffmpeg -i %%A -c:v h264_nvenc -b:v 25M -maxrate 30M -pix_fmt yuv420p -sn -dn -an %%~nA___temp.ts
+    ffmpeg -i %%~nA___temp.ts -c:v copy %%~nA___temp_ns.mp4
+    ffmpeg -i %%~nA___temp_ns.mp4 -c:v h264_nvenc -b:v 25M -maxrate 30M -pix_fmt yuv420p -vf scale=%width%:-1 %%~nA_H264.mp4
+    del %%~nA___temp.ts
+    del %%~nA___temp_ns.mp4
 )
